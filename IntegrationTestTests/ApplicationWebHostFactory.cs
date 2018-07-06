5619513426c5c35884fe97a505657340cd9ee7ed
Ifthekhar <sujoy18@yahoo.com>
2018-07-06 10:59:31 +1000

    test commit for teamcity

---
+++ IntegrationTestTests/ApplicationWebHostFactory.cs
@@ -0,0 +1,22 @@
+﻿using IntegrationTest;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace IntegrationTestTests
+{
+    public class ApplicationWebHostFactory<TStartp> : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+        }
+        protected override void ConfigureClient(HttpClient client)
+        {
+            base.ConfigureClient(client);
+        }
+    }
+}
---
+++ IntegrationTestTests/HomeControllerIntegrationTests.cs
@@ -0,0 +1,32 @@
+﻿using IntegrationTest;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTestTests
+{
+    public class HomeControllerIntegrationTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        public HomeControllerIntegrationTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/api/Home")]
+        [InlineData("/api/Values")]
+        public async Task Should_Receive_Success_Response(string url)
+        {
+            //Arrange
+            var client = _factory.CreateClient();
+            //Act
+            var response = await client.GetAsync(url);
+            //Assert
+            response.EnsureSuccessStatusCode();            
+            Assert.Equal("application/json; charset=utf-8",
+           response.Content.Headers.ContentType.ToString());
+        }
+    }
+    
+}
